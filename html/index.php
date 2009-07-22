c83bb6fabda8b8687b068e6294f0bef0462e0492
krystof1000 <krystof1000@d53c7cf4-70a9-11de-baec-69240fdc299a>
2009-07-22 21:04:00 +0000

    Vytvorena trida se sloupcovym a liniovym grafem, jeji konstruktor poskytuje prijemne rozhrani pro vytvareni grafu.

--- html/index.php
+++ html/index.php
@@ -2,11 +2,11 @@
 //  ini_set("session.use_cookies_only","1");
 //  session_cache_expire(60);
 session_start();
-$info = "";
+$info1 = "Please login.";
 if (isset($_REQUEST['logout'])) {
     unset($_SESSION['user']);
     session_destroy();
-    $info = "You were successfully logged out.";
+    $info1 = "You were successfully logged out.";
 }
 
 if (isset($_SESSION['user'])) {
@@ -48,10 +48,10 @@ if( $code == 1 ) {
                 <h2 class="section">Welcome to AEISEC CR LC Performance Monitor</h2>
                 <div class="content">
                     <p>Apedog is a tool, which helps LCs in the Czech Republic to track their plans. It was developed to ensure sustainable growth, improving KPIs and improving AIESEC experience.</p>
-                    <b>Please login</b>
+                    <b><?php echo $info1; ?></b>
 
                     <form action="index.php" method="post"><div align="left" >
-                            <?php echo $info; ?>
+                            
                             <table style="margin: 0pt; padding: 10px; text-align: left; border:0">
                                 <tr><td><label for="userid">Login: </label></td><td><input type="text" name="userid" id="userid" size="10" /></td></tr>
                                 <tr><td><label for="userpass">Password: </label></td><td><input type="password" name="userpass" id="userpass" size="10" /></td></tr>
