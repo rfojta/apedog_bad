54c3acfaee4a4a137e45a63f96f8e16955aa4dd0
rfojta <rfojta@d53c7cf4-70a9-11de-baec-69240fdc299a>
2009-07-28 16:10:21 +0000

    Upraveno Lc a User konfigurace, tak aby se pod uzivatelem zobrazoval seznam LC

--- html/classes/controller/lc_controller.php
+++ html/classes/controller/lc_controller.php
@@ -10,10 +10,25 @@
  * @author Richard
  */
 class LcController extends GenericController {
-    //put your code here
+//put your code here
 
     protected function get_row_label( $row ) {
         return $row['name'] . " - " . $row['description'];
+    }
+
+    public function get_list_box($id, $selected) {
+        $rows = $this->model->find_all();
+        echo "<select name=\"$id-lc\">";
+        foreach( $rows as $row ) {
+            echo "<option value=\"" . $row['id'] . "\"";
+            if( $row[id] == $selected ) {
+                echo "selected=\"1\"";
+            }
+            echo ">";
+            echo $this->get_row_label($row)
+                . "</option>";
+        }
+        echo "</select>";
     }
 }
 ?>
--- html/classes/controller/user_controller.php
+++ html/classes/controller/user_controller.php
@@ -12,8 +12,39 @@
 class UserController extends GenericController {
     //put your code here
 
+    protected $lc;
+    protected $lc_link = 'lc_conf.php';
+
+    function  __construct($model, $lc) {
+        parent::__construct($model);
+        $this->lc = $lc;
+    }
+
     protected function get_row_label( $row ) {
         return $row['login'] . " - " . $row['lc'];
+    }
+
+    private function lc_list($id, $selected) {
+        $a = $this->lc;
+        echo "<span title=\"select superior LC for this User\">LC: </span>";
+        if( isset( $this->request['lc']) ) {
+            $a->get_list_box($id, $this->request['lc']);
+
+        } else {
+            $a->get_list_box($id, $selected);
+        }
+        if( $selected > 0 ) {
+            $link = $this->lc_link;
+            echo "(<a href=\"$link?id=$selected\">". $a->get_label($selected) ."</a>)<br>";
+        }
+    }
+
+    protected function edit_item_row($id, $key, $value) {
+        if( $key == 'lc') {
+            $this->lc_list($id, $value);
+        } else {
+            parent::edit_item_row($id, $key, $value);
+        }
     }
 }
 ?>
