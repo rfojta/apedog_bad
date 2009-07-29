8cbe32f18e2724bba262ef5a9f2e496cc812afda
rfojta <rfojta@d53c7cf4-70a9-11de-baec-69240fdc299a>
2009-07-29 00:36:44 +0000

    vytvoren wrapper admin.php, ktery vola stranky z podadresare administration, z duvodu statickeho linkovani css a obrazku z /

--- trunk/html/administration/area_conf.php
+++ trunk/html/administration/area_conf.php
@@ -7,52 +7,22 @@
 session_cache_expire(60);
 session_start();
 if (!isset($_SESSION['user'])) { header("Location: index.php"); exit; }
+//ini_set('include_path',
+//  ini_get('include_path') . PATH_SEPARATOR . '../');
+//chdir('..');
 include('init.php');
-
 
 $area_model = new AreaModel($dbutil);
 $kpi_model = new KpiModel($dbutil);
-$area = new AreaController($area_model, $kpi_model);
+$controller = new AreaController($area_model, $kpi_model);
 
-if( isset( $_POST['posted'])) {
-    $area->submit( $_POST );
-}
+$page_title = "Apedog: Area and KPI configuration";
+$page_help = "<h3>KPI Configuration</h3>
+<p>you can add, modify, remove KPI or Area</p>
+<p>Each KPI belongs to one Area</p>";
+$page = 'configuration';
+
+include('components/basic_form.php');
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html>
-    <head>
-        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-        <title>Apedog: Main Page</title>
-        <meta name="keywords" content="" />
-        <meta name="description" content="" />
-        <link href="default.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-        <?php include('menu.php'); ?>
-        <div id="content">
-            <div id="colOne">
-                <h2 class="section">KPI Configuration</h2>
-                <div class="content">
-                    <form method="POST" action="">
-                        <?php $area->get_form_content($_REQUEST); ?>
-                        <p>
-                            <input type="hidden" name="posted" value="1" />
-                            <input type=submit />
-                        </p>
-                    </form>
-                </div>
-            </div>
-            <div id="colTwo">
-                <h2 class="section">Help</h2>
-                <div class="content">
-                    <h3>KPI Configuration</h3>
-                    <p>you can add, modify, remove KPI or Area</p>
-                    <p>Each KPI belongs to one Area</p>
-                </div>
-            </div>
-            <div style="clear: both;">&nbsp;</div>
-        </div>
-        <?php include('footer.php'); ?>
-    </body>
-</html>
+
