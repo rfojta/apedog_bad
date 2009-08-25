e2861c64594dec064711a55ee00374afb83841c6
rfojta <rfojta@d53c7cf4-70a9-11de-baec-69240fdc299a>
2009-08-25 09:38:44 +0000

    rozsireni modelu o ddl

--- trunk/html/classes/model/lc_model.class.php
+++ trunk/html/classes/model/lc_model.class.php
@@ -36,4 +36,56 @@ class LcModel extends GenericModel {
     }
 
 }
+
+/*
+-- phpMyAdmin SQL Dump
+-- version 3.1.3.1
+-- http://www.phpmyadmin.net
+--
+-- Počítač: localhost
+-- Vygenerováno: Úterý 25. srpna 2009, 11:25
+-- Verze MySQL: 5.0.45
+-- Verze PHP: 5.2.9
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Databáze: `apedog2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `lcs`
+--
+
+CREATE TABLE IF NOT EXISTS `lcs` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `name` varchar(45) character set utf8 collate utf8_bin NOT NULL,
+  `description` varchar(45) character set utf8 collate utf8_bin NOT NULL,
+  `created` datetime NOT NULL,
+  `updated` datetime NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `id` (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+
+--
+-- Vypisuji data pro tabulku `lcs`
+--
+
+INSERT INTO `lcs` (`id`, `name`, `description`, `created`, `updated`) VALUES
+(1, 'Praha', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(2, 'Brno', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(3, 'Ostrava', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(4, 'Olomouc', 'syrecky', '0000-00-00 00:00:00', '2009-07-29 02:39:40'),
+(5, 'Zlín', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(6, 'ČZU Praha', '', '0000-00-00 00:00:00', '2009-07-28 18:00:54'),
+(7, 'Pardubice', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(8, 'Plzeň', '', '0000-00-00 00:00:00', '2009-07-28 18:01:03'),
+(9, 'Karviná', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(10, 'MC', 'Member Committee  Czech republic', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+
+</pre>
+
+*/
 ?>
--- trunk/html/classes/model/user_model.class.php
+++ trunk/html/classes/model/user_model.class.php
@@ -37,4 +37,56 @@ class UserModel extends GenericModel {
         return $row['login'] . " - " . $row['lc'];
     }
 }
-?>
+
+/*
+-- phpMyAdmin SQL Dump
+-- version 3.1.3.1
+-- http://www.phpmyadmin.net
+--
+-- Počítač: localhost
+-- Vygenerováno: Úterý 25. srpna 2009, 11:27
+-- Verze MySQL: 5.0.45
+-- Verze PHP: 5.2.9
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Databáze: `apedog2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `name` varchar(45) collate utf8_bin NOT NULL,
+  `surname` varchar(45) collate utf8_bin NOT NULL,
+  `lc` int(10) unsigned NOT NULL,
+  `pass` varchar(45) collate utf8_bin default NULL,
+  `login` varchar(45) collate utf8_bin NOT NULL,
+  `created` datetime NOT NULL,
+  `updated` datetime NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `users-lc` (`lc`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=11 ;
+
+--
+-- Vypisuji data pro tabulku `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `surname`, `lc`, `pass`, `login`, `created`, `updated`) VALUES
+(1, 'Marek', 'Beran', 1, 'brucelee', 'Praha', '0000-00-00 00:00:00', '2009-07-28 17:52:50'),
+(2, '', '', 3, 'brucelee', 'Ostrava', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(3, '', '', 8, 'brucelee', 'Plzen', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(4, '', '', 7, 'brucelee', 'Pardubice', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(5, '', '', 2, 'brucelee', 'Brno', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(6, '', '', 6, 'brucelee', 'CZU', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(7, '', '', 9, 'brucelee', 'Karvina', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(8, '', '', 5, 'brucelee', 'Zlin', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(9, '', '', 4, 'brucelee', 'Olomouc', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(10, '', '', 10, 'brucelee', 'MC', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+
+*/
