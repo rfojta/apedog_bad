1d29e96c3c5bce675032741fdb804ed235d84dc4
rfojta <rfojta@d53c7cf4-70a9-11de-baec-69240fdc299a>
2010-07-26 22:14:57 +0000

    Modification on BSC view

--- branches/1.5/html/classes/View.class.php
+++ branches/1.5/html/classes/View.class.php
@@ -14,11 +14,18 @@ class BSC_View {
     private $dbutil;
     private $csfs;
     private $user;
+    private $query;
     
     function __construct($dbutil, $csfs, $user) {
         $this->dbutil = $dbutil;
         $this->csfs = $csfs;
         $this->user = $user;
+        $this->query = "select s.name strategy, sa.name action, "
+            . " o.name operation, r.name responsible, o.when, o.status"
+            . " from bsc_strategy s join bsc_strategic_action sa on (sa.strategy = s.id) "
+            . " join bsc_operations o on (o.strategic_action = sa.id) "
+            . " join bsc_responsible r on (o.responsible = r.id) "
+            . " where s.csfs = ";
     }
     
     /**
@@ -26,11 +33,9 @@ class BSC_View {
      * operations, actions and responsible
      */
     function get_form_content() {
-        $select = "select s.name strategy, sa.name action "
-            . "from bsc_strategy s join bsc_strategic_action sa on (sa.strategy = s.id) "
-            . " where s.csfs = " . $this->csfs;
+        $select = $this->query . $this->csfs;
         $rows = $this->dbutil->process_query_assoc($select);
-        echo "<table><tr>";
+        echo "<table class=\"view\"><tr>";
         foreach( $rows[0] as $key => $value ) {
             echo "<th>" . htmlspecialchars($key) . "</th>";
         }
