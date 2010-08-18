d632da31e7d5c90be6c0b53119ccc26ee67f5e0d
krystof1000 <krystof1000@d53c7cf4-70a9-11de-baec-69240fdc299a>
2010-08-18 15:21:56 +0000

    unique constraint nad users.login

--- db/bsc/bsc_alters.sql
+++ db/bsc/bsc_alters.sql
@@ -1,3 +1,4 @@
 ALTER TABLE  `bsc_operations` CHANGE  `status`  `status` INT( 1 ) NOT NULL DEFAULT  '0';
 ALTER TABLE  `bsc_operations` ADD  `last_change` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
 ALTER TABLE  `bsc_responsible` ADD  `email` VARCHAR( 48 ) NOT NULL;
+alter table `users` add constraint unique_login UNIQUE (login);
