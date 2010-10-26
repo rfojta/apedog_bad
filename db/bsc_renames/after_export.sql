f85d9aae0dee3689e56510ddcdea99ad2101746d
krystof1000 <krystof1000@d53c7cf4-70a9-11de-baec-69240fdc299a>
2010-10-26 16:17:01 +0000

    -sjednocena db -zprovoznene zalohovani a maily

--- db/bsc_renames/after_export.sql
+++
@@ -1,6 +0,0 @@
-update bsc_strategy set csfs = (select id from csfs where shortcut = csf_name limit 0,1);
-
-update bsc_operation
-set `when` = str_to_date(when_txt, '%d.%m.%Y')
-WHERE `when_txt` REGEXP '[[:digit:]]{2}\.[[:digit:]]{1,2}\.[[:digit:]]{4}';
-
