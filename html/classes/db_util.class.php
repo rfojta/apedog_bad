361a58628576082374a1d1b9a4ca98a09b16b3dc
rfojta@gmail.com <rfojta@gmail.com@d53c7cf4-70a9-11de-baec-69240fdc299a>
2012-03-05 12:09:29 +0000

    Purifying

--- html/classes/db_util.class.php
+++ html/classes/db_util.class.php
@@ -1,11 +1,7 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
- * Description of db_utilclass
+ * Description of db_util class
  *
  * @author Richard
  */
